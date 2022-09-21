884c34e4bfb5d8b62a0d99b31d4b2e9724171af8
Yaroslav Baiduk <byaa99@gmail.com>
2022-09-21 18:08:18 +0300

    Upload

---
+++ index.js
@@ -0,0 +1,19 @@
+$(function() {
+  $('.open').click(function() {
+    $('.form').css('display', 'block');
+  });
+  
+  $('.form__btn').click(function(event) {
+    event.preventDefault();
+    const name = $('.form__name').val();
+    const email = $('.form__email').val();
+    const phone = $('.form__phone').val();
+
+    if (name.length > 2 && email.length > 2 && phone.length > 2) {
+      $('.form').css('display', 'none');
+      $('body').append('<h1>Form submitted successfully</h1>')
+    } else {
+      $('.form__name-error').css('visibility', 'visible');
+    }
+  });
+});
